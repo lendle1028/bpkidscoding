250f342fa54ab497ce89bdaae06e9ed6c19dff4d
lendle <lendle_tseng@seed.net.tw>
2019-08-01 07:02:45 +0800

    Merge origin/master
    Conflicts:
    	src/main/webapp/WEB-INF/db.log
    	src/main/webapp/WEB-INF/db.properties
    	src/main/webapp/WEB-INF/db.script

--- src/main/java/rocks/imsofa/bp/kidscoding/editor/service/impl/StoryBookServiceImpl.java
+++ src/main/java/rocks/imsofa/bp/kidscoding/editor/service/impl/StoryBookServiceImpl.java
@@ -5,10 +5,45 @@
  */
 package rocks.imsofa.bp.kidscoding.editor.service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Service;
+import rocks.imsofa.bp.kidscoding.editor.model.StoryBook;
+import rocks.imsofa.bp.kidscoding.editor.service.StoryBookService;
+
 /**
  *
  * @author lendle
  */
-public class StoryBookServiceImpl {
+@Service
+public class StoryBookServiceImpl implements StoryBookService{
+    @Autowired
+    private JdbcTemplate jdbcTemplate=null;
+
+    public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+    
+    
+    @Override
+    public StoryBook getStoryBook(int id) {
+        jdbcTemplate.queryForRowSet("select * from story,users  left join users on story.author=users.user_id, storycontent where story.id=storycontent.story and story.id=? order by page", id);
+        return null;
+    }
+
+    @Override
+    public boolean addStoryBook(StoryBook storyBook) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean updateStoryBook(StoryBook storyBook) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean deleteStoryBook(int id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
